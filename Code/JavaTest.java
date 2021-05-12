784df5b663a38ea561c54a49d2146d8864a34684
chenxinhui <1198039762@qq.com>
2021-05-12 17:13:51 +0800

    trans-init

--- Code/JavaTest.java
+++ Code/JavaTest.java
@@ -1,14 +1,13 @@
-/**
- * Program: Hello world;<br>
- * Time: 2015/12/04.
- * @author Stick Cui
- */
-public class JavaTest{
-	/**
-	 * The main methord.
-	 * @param args The parameter when the methord is called.
-	 */
-	public static void main(String[] args){
-		System.out.println("Hello world!");
-	}
-}
+<?xml version="1.0" encoding="UTF-8"?>
+<beans xmlns="http://www.springframework.org/schema/beans"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://www.springframework.org/schema/beans
+  https://www.springframework.org/schema/beans/spring-beans.xsd">
+  <bean id="..." class="..."> ① ②
+  <!-- collaborators and configuration for this bean go here -->
+  </bean>
+  <bean id="..." class="...">
+  <!-- collaborators and configuration for this bean go here -->
+  </bean>
+  <!-- more bean definitions go here -->
+</beans>
